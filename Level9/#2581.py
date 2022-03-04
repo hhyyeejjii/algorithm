a6ae67df5e39644a0b931d73a3d06775a30349fb
hhyyeejjii <m_99_m@naver.com>
2022-03-05 01:10:41 +0900

    0304

---
+++ Level9/#2581.py
@@ -0,0 +1,17 @@
+#소수 2581
+m = int(input())
+n = int(input())
+decimalnum = []
+for i in range(m,n+1):
+    count = 0
+    for j in range(1, i+1):
+        if i % j == 0:
+            count += 1
+            if count > 2: break
+    if count == 2:
+        decimalnum.append(i)
+if len(decimalnum) != 0:
+    print(sum(decimalnum))
+    print(decimalnum[0])
+else:
+    print('-1')

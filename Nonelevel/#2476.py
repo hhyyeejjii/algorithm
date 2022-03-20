1cfa1a1e5dfbd2eb6a4b03d27e968fb974cc1f64
hhyyeejjii <m_99_m@naver.com>
2022-03-20 23:29:12 +0900

    0320

---
+++ Nonelevel/#2476.py
@@ -0,0 +1,20 @@
+#주사위 게임 2476
+
+dice = int(input())
+answer = 0
+
+for _ in range(dice):
+    a, b, c = map(int,input().split())
+
+    if a == b == c:
+        answer = max(answer, 10000+a*1000)
+    elif a == b:
+        answer = max(answer, 1000+a*100)
+    elif a == c:
+        answer = max(answer, 1000+a*100)
+    elif b == c:
+        answer = max(answer, 1000+b*100)
+    else:
+        answer = max(answer, 100 * max(a,b,c))
+
+print(answer)

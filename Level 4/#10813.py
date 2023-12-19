425ab5017232e947056d7afae54aa34d60e1f53d
hhyyeejjii <m_99_m@naver.com>
2023-12-19 14:55:25 +0900

    231219

---
+++ Level 4/#10813.py
@@ -0,0 +1,8 @@
+#10813번 공 바꾸기
+print(*arr[1:])
+n, m = map(int, input().split()) # n = 바구니 갯수, m = 공 바꾸기 횟수
+basket = list(range(n+1)) #0번부터 
+for _ in range(m):
+    i,j = map(int,input().split())
+    basket[i] , basket[j] = basket[j] , basket[i] # i, j 교환
+print(*basket[1:]) # *으로 데이터만, 1번부터인 이유는 0번에 0 출력
